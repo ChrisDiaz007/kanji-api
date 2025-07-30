793bba1bd6b301b0499e1d7a5d22f3b56ee20690
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-07-30 09:38:41 -0400

    Added Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_kanjis
+  has_many :kanjis, through: :user_kanjis
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
