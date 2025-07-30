3bf38100747dd69d3af509abee6091b12c589d9c
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-07-30 09:41:22 -0400

    Created Kanji User_kanjis controller

---
+++ app/controllers/api/v1/kanjis_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::KanjisController < ApplicationController
+  def index
+    @kanjis = Kanji.all
+    if params[:character].present?
+      @kanjis = Kanji.where('character ILIKE ?', "%#{params[:character]}%")
+    else
+      @kanjis = Kanji.all
+    end
+    render json: @kanjis
+  end
+
+  def show
+    @kanji = Kanji.find_by(character: params[:id])
+    if @kanji
+      render json: @kanji
+    else
+      render json: { error: "Kanji not found" }, status: :not_found
+    end
+  end
+end
