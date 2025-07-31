2ba99bb122ada1979d3e2c700451097ceb578e2c
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-07-31 11:00:16 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'api/v1/registrations',
+    sessions: 'api/v1/sessions'
+  }
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
